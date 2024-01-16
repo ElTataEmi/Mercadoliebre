46e5be5d8d094b4f1d4a10f3b3503f70d1930038
ElTataEmi <turasis88@gmail.com>
2024-01-15 22:35:12 -0300

    subo structure

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const path = require("path");
+
+const app = express();
+const port = 3030;
+
+
+app.use(express.static('public'))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"))
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))

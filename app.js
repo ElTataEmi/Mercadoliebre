c11df839dcc94d47159655bd9e92dc7c73a6cf08
ElTataEmi <turasis88@gmail.com>
2024-01-25 00:15:46 -0300

    Se crean Registro.html y login.html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const path = require("path");
 
 const app = express();
-const port = 3030;
+const port = 3000;
 
 
 app.use(express.static('public'))
@@ -12,9 +12,12 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'))
 })
 
+app.get("/Register", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
 
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "about.html"))
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
 })
 
 app.listen(port, () => console.log(`http://localhost:${port}`))
